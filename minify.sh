4b10083e58b424d3202e9f9a8e20f42c7045ee0d
petersirka <petersirka@gmail.com>
2024-06-12 10:31:21 +0200

    Renamed files.

--- minify.sh
+++ minify.sh
@@ -2,5 +2,5 @@ echo "Minifying ..."
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 uglifyjs library.js --config-file minify.json -o library.min.js
-cat helpers.js library.min.js > ui.min@20.js
+cat helpers.js library.min.js > spa.min@20.js
 rm library.min.js
