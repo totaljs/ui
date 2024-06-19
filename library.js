2b3cb5a1e9137d220ca446c536e03d4fb040cb0a
petersirka <petersirka@gmail.com>
2024-06-19 13:41:15 +0200

    Added `String.parseDate()` method.

--- library.js
+++ library.js
@@ -3562,6 +3562,185 @@
 			}
 		};
 
+		function parseDateFormat(format, val) {
+
+			var tmp = [];
+			var tmpformat = [];
+			var prev = '';
+			var prevformat = '';
+			var allowed = { y: 1, Y: 1, M: 1, m: 1, d: 1, D: 1, H: 1, s: 1, a: 1, w: 1 };
+
+			for (var i = 0; i < format.length; i++) {
+
+				var c = format.charAt(i);
+
+				if (!allowed[c])
+					continue;
+
+				if (prev !== c) {
+					prevformat && tmpformat.push(prevformat);
+					prevformat = c;
+					prev = c;
+				} else
+					prevformat += c;
+			}
+
+			prev = '';
+
+			for (var i = 0; i < val.length; i++) {
+				var code = val.charCodeAt(i);
+				if (code >= 48 && code <= 57)
+					prev += val.charAt(i);
+			}
+
+			prevformat && tmpformat.push(prevformat);
+
+			var f = 0;
+			for (var i = 0; i < tmpformat.length; i++) {
+				var l = tmpformat[i].length;
+				tmp.push(prev.substring(f, f + l));
+				f += l;
+			}
+
+			var dt = {};
+
+			for (var i = 0; i < tmpformat.length; i++) {
+				var type = tmpformat[i];
+				if (tmp[i])
+					dt[type.charAt(0)] = +tmp[i];
+			}
+
+			var h = dt.h || dt.H;
+
+			if (h != null) {
+				var ampm = val.match(/am|pm/i);
+				if (ampm && ampm[0].toLowerCase() === 'pm')
+					h += 12;
+			}
+
+			var y = (dt.y || dt.Y) || 0;
+
+			if (y < 100)
+				y += 2000;
+
+			return new Date(y, (dt.M || 1) - 1, dt.d || dt.D || 0, h || 0, dt.m || 0, dt.s || 0);
+		}
+
+		PROTO.parseDate = function(format) {
+
+			if (format)
+				return parseDateFormat(format, this);
+
+			var self = this.trim().env();
+			if (!self)
+				return null;
+
+			var lc = self.charCodeAt(self.length - 1);
+
+			// Classic date
+			if (lc === 41)
+				return new Date(self);
+
+			// JSON format
+			if (lc === 90)
+				return new Date(Date.parse(self));
+
+			var ampm = null;
+			var tmp;
+
+			self = self.replace(/(\s)(am|pm)/i, function(text) {
+				ampm = text.trim().toLowerCase();
+				return '';
+			});
+
+			var arr = self.indexOf(' ') === -1 ? self.split('T') : self.split(' ');
+			var index = arr[0].indexOf(':');
+			var length = arr[0].length;
+
+			if (index !== -1) {
+				tmp = arr[1];
+				arr[1] = arr[0];
+				arr[0] = tmp;
+			}
+
+			if (arr[0] === undefined)
+				arr[0] = '';
+
+			var noTime = arr[1] === undefined ? true : arr[1].length === 0;
+
+			for (var i = 0; i < length; i++) {
+				var c = arr[0].charCodeAt(i);
+				if ((c > 47 && c < 58) || c === 45 || c === 46)
+					continue;
+				if (noTime)
+					return new Date(self);
+			}
+
+			if (arr[1] === undefined)
+				arr[1] = '00:00:00';
+
+			var firstDay = arr[0].indexOf('-') === -1;
+
+			var date = (arr[0] || '').split(firstDay ? '.' : '-');
+			var time = (arr[1] || '').split(':');
+			var parsed = [];
+
+			if (date.length < 4 && time.length < 2)
+				return new Date(self);
+
+			index = (time[2] || '').indexOf('.');
+
+			// milliseconds
+			if (index !== -1) {
+				time[3] = time[2].substring(index + 1);
+				time[2] = time[2].substring(0, index);
+			} else
+				time[3] = '0';
+
+			if (ampm) {
+				// 12 hours time
+				tmp = +time[0];
+				if (ampm === 'pm')
+					time[0] = tmp + 12;
+			}
+
+			parsed.push(+date[firstDay ? 2 : 0]); // year
+			parsed.push(+date[1]); // month
+			parsed.push(+date[firstDay ? 0 : 2]); // day
+			parsed.push(+time[0]); // hours
+			parsed.push(+time[1]); // minutes
+			parsed.push(+time[2]); // seconds
+			parsed.push(+time[3]); // miliseconds
+
+			var def = W.NOW = new Date();
+
+			for (var i = 0; i < parsed.length; i++) {
+				if (isNaN(parsed[i]))
+					parsed[i] = 0;
+
+				var value = parsed[i];
+				if (value !== 0)
+					continue;
+
+				switch (i) {
+					case 0:
+						if (value <= 0)
+							parsed[i] = def.getFullYear();
+						break;
+					case 1:
+						if (value <= 0)
+							parsed[i] = def.getMonth() + 1;
+						break;
+					case 2:
+						if (value <= 0)
+							parsed[i] = def.getDate();
+						break;
+				}
+			}
+
+			return new Date(parsed[0], parsed[1] - 1, parsed[2], parsed[3], parsed[4], parsed[5]);
+		};
+
 	})();
 
 	// Number prototypes
